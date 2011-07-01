54e1dedbcf5e1a28b8392cf342837454ec69dd49
Don Porter <porter@cs.stonybrook.edu>
2011-06-30 21:35:42 -0400

    lab 5 source.

--- kern/init.c
+++ kern/init.c
@@ -45,12 +45,17 @@ i386_init(void)
 	// Should always have an idle process as first one.
 	ENV_CREATE(user_idle);
 
+	// Start fs.
+	ENV_CREATE(fs_fs);
+
 #if defined(TEST)
 	// Don't touch -- used by grading script!
 	ENV_CREATE2(TEST, TESTSIZE);
 #else
 	// Touch all you want.
-	ENV_CREATE(user_primes);
+	// ENV_CREATE(user_writemotd);
+	// ENV_CREATE(user_testfile);
+	// ENV_CREATE(user_icode);
 #endif // TEST*
 
 	// Schedule and run the first user environment!
