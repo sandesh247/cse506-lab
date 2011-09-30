3f52e35549840940edff699b896f2ef1f2f5931b
Dhruv <dhruvbird@gmail.com>
2011-09-30 16:59:00 -0700

    Added debug to syscall()

--- kern/syscall.c
+++ kern/syscall.c
@@ -73,6 +73,7 @@ syscall(uint32_t syscallno, uint32_t a1, uint32_t a2, uint32_t a3, uint32_t a4, 
 	// Call the function corresponding to the 'syscallno' parameter.
 	// Return any appropriate return value.
 	// LAB 3: Your code here.
+	cprintf("syscall(%u, %u, %u)\n", syscallno, a1, a2);
 
 	switch (syscallno) {
 	case SYS_cputs:
