969da25f2e8d663a6c08875ff422e05b141bd24d
Sandesh Singh <sssingh@cs.stonybrook.edu>
2011-10-20 17:23:20 -0400

    missed setting the user_mem_check_addr

--- kern/pmap.c
+++ kern/pmap.c
@@ -848,6 +848,8 @@ int
 user_mem_check(struct Env *env, const void *va, size_t len, int perm)
 {
 	// LAB 3: Your code here.
+	user_mem_check_addr = 0;
+
 	uintptr_t start = ROUNDDOWN((uint32_t) va, PGSIZE);
 	uintptr_t end = (uintptr_t)(va) + len;
 	perm = perm | PTE_P; // ha
@@ -857,7 +859,10 @@ user_mem_check(struct Env *env, const void *va, size_t len, int perm)
 		DPRINTF("user_mem_check::checking %x\n", mem);
 		pte_t *pte = pgdir_walk(env->env_pgdir, (void *) mem, 0);
 
-		if(!pte) return -E_FAULT;
+		if(!pte) {
+			user_mem_check_addr = mem < (uintptr_t)(va) ? (uintptr_t)(va) : mem;
+			return -E_FAULT;
+		}
 
 		int perm_present = (*pte) ^ PTE_ADDR(*pte);
 
