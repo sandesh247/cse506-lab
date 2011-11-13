9759779188c39597ccce1e1aa212ed18201b6708
Dhruv <dhruvbird@gmail.com>
2011-11-13 11:14:15 -0800

    Fix merge conflict arrows that were left in the file

--- kern/init.c
+++ kern/init.c
@@ -90,7 +90,6 @@ i386_init(void)
 	// ENV_CREATE(user_primes);
 	// ENV_CREATE(user_testfile);
 	ENV_CREATE(user_icode);
->>>>>>> lab5
 #endif // TEST*
 
 	// Schedule and run the first user environment!
