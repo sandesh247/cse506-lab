0f474f63336616b349e7e5b4a4ab8459d5bb4d36
Dhruv <dhruvbird@gmail.com>
2011-12-02 17:14:07 -0800

    Fix ripc_recv()'s signature

--- inc/lib.h
+++ inc/lib.h
@@ -143,7 +143,7 @@ void	wait(envid_t env);
 // migrate.c
 int     migrate();
 int     ripc_send(int pid, void *va, int len, char *rbuff, int *rlen);
-int     ripc_recv(void *va, int len);
+int     ripc_recv(void *va, int len, char *sbuff, int *slen);
 
 
 /* File open modes */
